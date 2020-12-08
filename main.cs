684a83bc7073a8ec18f4818ab43ac558a4ec3332
mehmetcemkaraca <75699262+mehmetcemkaraca@users.noreply.github.com>
2020-12-09 01:12:58 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -8,7 +8,7 @@ class MainClass {
   public static void Main (string[] args) {
     Console.WriteLine ("Önerme sayısını giriniz:"); //önerme sayısını aldırdık örnek r,p gibi.
     double onerme;
-    onerme = Convert.ToDouble(Console.ReadLine()); // math kullanimi için double çevirdik
+    onerme = Convert.ToDouble(Console.ReadLine()); // math kullanimi için double çevirdik ve kullanicidan değer aldik.
     double top;
     top= Math.Pow(2,onerme); // math sınıfı kullanarak üssü aldırdık.
     
